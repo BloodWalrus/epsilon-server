2b7498ea37da910615a39555e82b52507c46f602
BloodWalrus <github.bmo12@simplelogin.com>
2022-09-26 15:56:42 +0100

    started

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod server;
 
+pub const SENSOR_COUNT: usize = 7;
+
 fn main() {
     println!("Hello, world!");
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,23 @@
+use std::error::Error;
+
+use crate::SENSOR_COUNT;
+use ecore::{connection::Connection, skeleton::Skeleton};
+use glam::Quat;
+
 pub struct Server {
-    streamer: Connection<
+    streamer: Connection<SENSOR_COUNT, Quat>,
+    skeleton: Skeleton,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Self {
+            streamer: todo!(),
+            skeleton: Default::default(),
+        }
+    }
+
+    pub fn main(mut self) -> Result<(), Box<dyn Error>> {
+        todo!()
+    }
 }
